5b812ae3bde02d4cab3ff6201f3bd242dbf512d7
DmitryKozinov <kozinov.fortech@gmail.com>
2025-04-08 10:58:03 +0300

    some commit

---
+++ src/test/java/ru/fortech/testjenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fortech.testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
