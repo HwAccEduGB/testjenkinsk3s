6f17c92519ed7a188efdf354a2855ee12dde699a
DmitryKozinov <kozinov.fortech@gmail.com>
2025-04-08 10:56:34 +0300

    first commit

---
+++ src/main/java/ru/fortech/testjenkins/controller/HelloController.java
@@ -0,0 +1,12 @@
+package ru.fortech.testjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello kuber test check v7 08.04.25 10-51";
+    }
+}
